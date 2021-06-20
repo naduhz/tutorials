ebd325f74b854f7e302a82d013fe959754e14e87
jordan <jleejx2000@gmail.com>
2021-06-20 21:32:11 +0800

    predicting completed

--- sentdex-machine-learning/regression-intro/regression-intro.py
+++ sentdex-machine-learning/regression-intro/regression-intro.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import numpy as np
 import math
+import datetime
 
 from sklearn import preprocessing
-from sklearn.model_selection import cross_validate, train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-df = pd.read_csv("regression-intro/dogecoin-historical-dataset/DOGE-USD.csv")
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use("ggplot")
+
+df = pd.read_csv(
+    "regression-intro/dogecoin-historical-dataset/DOGE-USD.csv",
+    parse_dates=True,
+    index_col="Date",
+)
 
 
 df["HL_PCT_change"] = (df["High"] - df["Low"]) / (df["Low"]) * 100.0
@@ -18,16 +27,19 @@ df = df[["Adj Close", "HL_PCT_change", "PCT_change", "Volume"]]
 forecast_col = "Adj Close"
 df.fillna(method="ffill", inplace=True)
 
-forecast_out = int(math.ceil(0.001 * len(df)))
+forecast_out = int(math.ceil(0.01 * len(df)))
 print(forecast_out)
 
 df["prediction"] = df[forecast_col].shift(-forecast_out)
-df.dropna(axis=0, inplace=True)
+
 
 X = np.array(df.drop(["prediction"], axis=1))
-y = np.array(df["prediction"])
-
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+
+df.dropna(axis=0, inplace=True)
 y = np.array(df["prediction"])
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -37,11 +49,27 @@ X_train, X_test, y_train, y_test = train_test_split(
 classifier = LinearRegression()
 classifier.fit(X_train, y_train)
 
-pred = classifier.predict(X_test)
-
 accuracy = classifier.score(X_test, y_test)
 
 print(accuracy)
-print(mean_absolute_error(y_test, pred))
-print(mean_squared_error(y_test, pred))
-print(np.sqrt(mean_squared_error(y_test, pred)))
+
+forecast_set = classifier.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+df["Forecast"] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for j in range(len(df.columns) - 1)] + [i]
+
+df["Adj Close"].plot()
+df["Forecast"].plot()
+plt.legend(loc=4)
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
