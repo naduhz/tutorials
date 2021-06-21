161a945275560cc20463e0befc72f919fb03437e
jordan <jleejx2000@gmail.com>
2021-06-21 19:20:49 +0800

    pickling

--- sentdex-machine-learning/regression-intro/regression-intro.py
+++ sentdex-machine-learning/regression-intro/regression-intro.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import datetime
+import pickle
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -28,7 +29,6 @@ forecast_col = "Adj Close"
 df.fillna(method="ffill", inplace=True)
 
 forecast_out = int(math.ceil(0.01 * len(df)))
-print(forecast_out)
 
 df["prediction"] = df[forecast_col].shift(-forecast_out)
 
@@ -48,11 +48,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 classifier = LinearRegression()
 classifier.fit(X_train, y_train)
+with open("linearregression.pickle", "wb") as f:
+    pickle.dump(classifier, f)
+
+pickle_in = open("linearregression.pickle", "rb")
+classifier = pickle.load(pickle_in)
 
 accuracy = classifier.score(X_test, y_test)
-
-print(accuracy)
-
 forecast_set = classifier.predict(X_lately)
 print(forecast_set, accuracy, forecast_out)
 
