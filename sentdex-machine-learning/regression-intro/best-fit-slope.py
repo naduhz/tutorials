d9a013d153306dffbe560247aeeb7586d866115b
jordan <jleejx2000@gmail.com>
2021-06-22 20:38:05 +0800

    rsquared theory

--- sentdex-machine-learning/regression-intro/best-fit-slope.py
+++ sentdex-machine-learning/regression-intro/best-fit-slope.py
@@ -19,9 +19,23 @@ def best_fit_slope_and_intercept(x, y):
     return m, b
 
 
+def squared_error(y_original, y_bestfitline):
+    return sum((y_bestfitline - y_original) ** 2)
+
+
+def coefficient_of_determination(y_original, y_bestfitline):
+    y_mean_line = [mean(y_original) for y in y_original]
+    squared_error_regression = squared_error(y_original, y_bestfitline)
+    squared_error_mean = squared_error(y_original, y_mean_line)
+    return 1 - (squared_error_regression / squared_error_mean)
+
+
 m, b = best_fit_slope_and_intercept(X, Y)
 print(m, b)
 regression_line = [(m * x) + b for x in X]
+
+r_squared = coefficient_of_determination(Y, regression_line)
+print(r_squared)
 
 plt.scatter(X, Y)
 plt.plot(X, regression_line, color="red")
