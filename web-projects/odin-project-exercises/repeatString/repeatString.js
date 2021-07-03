c5bfcfbbb28c9aeaacdaa01423cc7ab36281e96f
jordan <jleejx2000@gmail.com>
2021-07-03 11:24:40 +0800

    repeatString completed

--- web-projects/odin-project-exercises/repeatString/repeatString.js
+++ web-projects/odin-project-exercises/repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+  let returnedString = "";
+  if (times < 0) {
+    return "ERROR";
+  }
+  for (let i = 1; i <= times; i++) {
+    returnedString += string;
+  }
+  return returnedString;
 };
 
 module.exports = repeatString;
