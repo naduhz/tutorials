ec5bb438a2ce4fc46e93c140c8b21acde268d926
jordan <jleejx2000@gmail.com>
2021-07-03 11:50:54 +0800

    removeFromArray completed

--- web-projects/odin-project-exercises/removeFromArray/removeFromArray.js
+++ web-projects/odin-project-exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  const array = args[0];
+  const newArray = [];
 
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
+
+// const removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter((val) => {!args.includes(val)})
+// }
 
 module.exports = removeFromArray;
