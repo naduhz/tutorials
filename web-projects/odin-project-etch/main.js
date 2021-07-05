4ac0a57a571af787cb475def26fe38a6d0c73ecb
jordan <jleejx2000@gmail.com>
2021-07-05 22:59:08 +0800

    changed to grid layout

--- web-projects/odin-project-etch/main.js
+++ web-projects/odin-project-etch/main.js
@@ -5,44 +5,22 @@ function gridReset() {
   }
 }
 
-for (let i = 0; i <= 16; i++) {
-  const row = document.createElement("div");
-  row.setAttribute("class", "rowContainer");
-  row.setAttribute("id", `rowContainer${i}`);
-  gridContainer.appendChild(row);
-}
+function makeGrid(rows, cols) {
+  gridContainer.style.setProperty("--grid-rows", rows);
+  gridContainer.style.setProperty("--grid-cols", cols);
 
-for (let row = 0; row <= 16; row++) {
-  const rowContainer = document.querySelector(`#rowContainer${row}`);
-  for (let column = 0; column <= 16; column++) {
+  for (let i = 1; i <= rows * cols; i++) {
     const square = document.createElement("div");
-    square.setAttribute("class", "square");
-    square.setAttribute("id", `square${row}${column}`);
-    rowContainer.appendChild(square);
+    gridContainer.appendChild(square).className = "square";
   }
 }
 
-const changeGridSizeButton = document.querySelector("#gridSize");
-changeGridSizeButton.addEventListener("click", (event) => {
-  const rows = prompt("Rows? (Limit: 100)");
-  const columns = prompt("Columns? (Limit: 100)");
+makeGrid(70, 70);
 
-  gridReset();
+// const changeGridSizeButton = document.querySelector("#gridSize");
+// changeGridSizeButton.addEventListener("click", (event) => {
+//   const rows = prompt("Rows? (Limit: 100)");
+//   const cols = prompt("Columns? (Limit: 100)");
 
-  for (let row = 0; row <= Number(rows) - 1; row++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.setAttribute("class", "rowContainer");
-    rowContainer.setAttribute("id", `rowContainer${row}`);
-    gridContainer.appendChild(rowContainer);
-  }
-
-  for (let row = 0; row <= Number(rows) - 1; row++) {
-    const rowContainer = document.querySelector(`#rowContainer${row}`);
-    for (let column = 0; column <= Number(columns) - 1; column++) {
-      const square = document.createElement("div");
-      square.setAttribute("class", "square");
-      square.setAttribute("id", `square${row}${column}`);
-      rowContainer.appendChild(square);
-    }
-  }
-});
+//   gridReset();
+// });
