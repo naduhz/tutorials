457240c6b397e84cc35d917e0c2e2a58aff7e801
jordan <jleejx2000@gmail.com>
2021-07-07 14:13:58 +0800

    created some miscellaneous buttons

--- web-projects/odin-project-etch/main.js
+++ web-projects/odin-project-etch/main.js
@@ -24,20 +24,24 @@ function makeGrid(rows, cols) {
 
 makeGrid(16, 16);
 
-const changeGridSizeButton = document.querySelector("#gridSize");
-changeGridSizeButton.addEventListener("click", (event) => {
-  do {
-    var rows = prompt("Rows? (Limit: 100)");
-    if (rows === null) return;
-  } while (!parseInt(rows) || parseInt(rows) > 100);
+const blackButton = document.querySelector("#black");
+blackButton.addEventListener("click", (event) => {
+  const squaresArray = Array.from(gridContainer.children);
+  squaresArray.forEach((square) => {
+    square.addEventListener("mouseover", (event) => {
+      square.style.backgroundColor = "#000000";
+    });
+  });
+});
 
-  do {
-    var cols = prompt("Columns? (Limit: 100)");
-    if (cols === null) return;
-  } while (!parseInt(cols) || parseInt(cols) > 100);
-
-  gridReset();
-  makeGrid(parseInt(rows), parseInt(cols));
+const eraserButton = document.querySelector("#eraser");
+eraserButton.addEventListener("click", (event) => {
+  const squaresArray = Array.from(gridContainer.children);
+  squaresArray.forEach((square) => {
+    square.addEventListener("mouseover", (event) => {
+      square.style.backgroundColor = "silver";
+    });
+  });
 });
 
 const randomizeColorButton = document.querySelector("#randomizeColor");
@@ -64,4 +68,28 @@ colorPicker.addEventListener("change", (event) => {
       square.style.backgroundColor = color;
     });
   });
+});
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", (event) => {
+  const squaresArray = Array.from(gridContainer.children);
+  squaresArray.forEach((square) => {
+    square.style.backgroundColor = "silver";
+  });
+});
+
+const changeGridSizeButton = document.querySelector("#gridSize");
+changeGridSizeButton.addEventListener("click", (event) => {
+  do {
+    var rows = prompt("Rows? (Limit: 100)");
+    if (rows === null) return;
+  } while (!parseInt(rows) || parseInt(rows) > 100);
+
+  do {
+    var cols = prompt("Columns? (Limit: 100)");
+    if (cols === null) return;
+  } while (!parseInt(cols) || parseInt(cols) > 100);
+
+  gridReset();
+  makeGrid(parseInt(rows), parseInt(cols));
 });
