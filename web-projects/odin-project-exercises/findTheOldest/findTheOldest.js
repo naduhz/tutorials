5015b50c3843aaefc5f8d5eab2abaf376b34da83
jordan <jleejx2000@gmail.com>
2021-07-08 17:13:39 +0800

    findTheOldest, getTheTitles completed

--- web-projects/odin-project-exercises/findTheOldest/findTheOldest.js
+++ web-projects/odin-project-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const sorted = array.sort((personA, personB) => {
+    let aAge;
+    let bAge;
+    let currentYear = new Date().getFullYear();
 
+    "yearOfDeath" in personA
+      ? (aAge = personA.yearOfDeath - personA.yearOfBirth)
+      : (aAge = currentYear - personA.yearOfBirth);
+
+    "yearOfDeath" in personB
+      ? (bAge = personB.yearOfDeath - personB.yearOfBirth)
+      : (bAge = currentYear - personB.yearOfBirth);
+
+    return aAge > bAge ? -1 : 1;
+  });
+
+  const oldest = sorted[0];
+  return oldest;
 };
 
 module.exports = findTheOldest;
