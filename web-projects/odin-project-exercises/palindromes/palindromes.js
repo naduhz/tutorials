d3fd16872a5fff083fcdb1d345dc21aeb154be1b
jordan <jleejx2000@gmail.com>
2021-07-08 16:21:36 +0800

    palindrome  done

--- web-projects/odin-project-exercises/palindromes/palindromes.js
+++ web-projects/odin-project-exercises/palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanedString = Array.from(string.toLowerCase())
+    .filter((character) => /[a-zA-Z]/.test(character))
+    .join();
+  const reversedString = Array.from(cleanedString).reverse().join("");
+  return cleanedString === reversedString ? true : false;
 };
 
 module.exports = palindromes;
