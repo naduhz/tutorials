df41ef1a8fec719ece3d80bfb8071e88ca07ae4d
jordan <jleejx2000@gmail.com>
2021-07-08 16:49:52 +0800

    fibonacci complete

--- web-projects/odin-project-exercises/fibonacci/fibonacci.js
+++ web-projects/odin-project-exercises/fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+var sequence = [];
 
+const fibonacci = function (number) {
+  if (parseInt(number) <= 0 || !parseInt(number)) {
+    return "OOPS";
+  }
+
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < number; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
 };
 
 module.exports = fibonacci;
